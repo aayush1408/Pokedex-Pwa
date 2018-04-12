7e33ef3e943f9590a51d3198ac7068078265aa03
aayush1408 <aayushjaiswal0114@gmail.com>
2018-04-13 00:01:54 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const main = document.querySelector('main');
 const stats = document.querySelector('#stats');
-
-
+const types = document.querySelector('#types');
+if ('serviceWorker' in navigator) {
+    try {
+        navigator.serviceWorker.register('sw.js');
+        console.log('SW registered');
+    }
+    catch (error) {
+        console.log('SW not registered');
+    }
+}
 function pokemonName() {
     const pokemon = document.querySelector('#pokemon');
     const pokemon_name = pokemon.value;
@@ -17,9 +25,13 @@ async function getPokemon(pokemon) {
         main.innerHTML = `<h2>This pokemon is yet to be known</h2>`
     }
     else {
+        console.log(json.types);
         main.innerHTML = viewPokemon(json);
         stats.innerHTML = json.stats.map(function (i) {
             return `<li class="collection-item"> ${i.stat.name} ----- ${i.base_stat}</li>`
+        }).join('\n');
+        types.innerHTML = json.types.map(function (i) {
+            return `<li class="collection-item">Type -- ${i.type.name}</li>`
         }).join('\n');
     }
 }
@@ -30,8 +42,10 @@ function viewPokemon(pokemon) {
         <div>
         <h5>Name -- ${pokemon.name}</h5>        
         <img src=${pokemon.sprites.front_default} />
-        <p>Weight -- ${pokemon.weight}</p>
-        <p>Height -- ${pokemon.height}</p>
+        <ul class="collection" style="border:none;">
+        <li class="collection-item">Weight -- ${pokemon.weight}</li>
+        <li class="collection-item">Height -- ${pokemon.height}</li>
+        </ul>
         </div>
     `
 }
